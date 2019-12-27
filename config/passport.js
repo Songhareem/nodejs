410a5504e4d3e8a714d277e0a3e305c4c14092b5
elaha00 <elaha00@naver.com>
2019-12-27 19:09:34 +0900

    start applying jwt

--- config/passport.js
+++ config/passport.js
@@ -15,10 +15,37 @@ module.exports = function() {
     passport.use(new localStorage({
         usernameField: "email",
         passportField: "password",
-    },
-    function (email, password, done) {
-        // 저장된 User와 비교
-    }
-    ))
+        },
+        function (email, password, done) {
+            // 저장된 User와 비교
+            return UserModel.findOne({where : {user_email:email, password: password}})
+            .then(function(user) {
+                if(!user) {
+                    return done(null, false, {message: 'incorrect email or password'});
+                }
+                return done(null, user, {message: 'Logged In Successfully'});
+            })
+            .catch(function(err) {
+                done(err);
+            })
+        }   
+    ));
+
+    // JWT Strategy
+    passport.use(new jwt_strategy({
+
+        jwtFromRequest: extra_jwt.fromAuthHeaderAsBearerToken(),
+        secretOrKey : "1q2w3e4r@"   // 나중에 env로 옮길 것
+        },
+            function(jwtPayload, done) {
+                return UserModel.findOneById(jwt_strategy.id)
+                .then(function(user) {
+                    return done(null, user);
+            })
+            .catch(function(err) {
+                return done(err);
+            })
+        }
+    ));
 }
 
