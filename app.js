aa0bdbe2e9b8663d0365e3bc57b796dd1952cb87
elaha00 <elaha00@naver.com>
2019-12-31 13:20:07 +0900

    session study

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const mysqlStore = require('express-mysql-session')(session);
 const pbkdf2password = require('pbkdf2-password');
 //const authToken = require("./controller/authTokenController");
 
+const passportLocal = require("passport-local");
+const LocalStrategy = passportLocal.Strategy;
+
 const app = express();
 const hasher = pbkdf2password();
 
@@ -54,26 +57,28 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 const users = [
+    
     {
-      username: 'junhyeok',
+      useremail: 'elaha00@naver.com',
       password: 'hWB/AM3MHngW2N0tphhwhSjH1FagibQamovfQCvo7v2sCfxhDoMv8NEtZjHy3hv5gU242zMpymWIkptqVP5BoocBczcKPLrQ9WNpO+I9NjBUxOMgBazjg649mjc+3kbIomkMSWDxvdxshCXbhNiTmD7iNVeCou/RXTmSDv9mZ8M=',
-      displayName: 'Jun',
+      name: 'Song',
       salt: 'nYMlZ4HIiz3A0LF2A9t4Hj8NdrdQm8OOLVKf8zoZj9NGOO0EDINhCJaxsocXflE3pBqMSATj5xY0nnmBHvg7IQ=='
     }
+    
 ]
 
 passport.serializeUser(function(user, done) {
     console.log('serializeUser', user);
-    done(null, user.username);
+    done(null, user.useremail);
 });
 
 //deserializeUser => serializeUser의 done(null, user.username);을 전달받음
-passport.deserializeUser(function (uname, done) {
-    console.log('deserializeUser', uname);
+passport.deserializeUser(function (uemail, done) {
+    console.log('deserializeUser', uemail);
     
     for(i=0; i<users.length; i++) {
         var user = users[i];
-        if(user.username == uname) {
+        if(user.useremail == uemail) {
             
             return done(null, user);
         }
@@ -86,34 +91,99 @@ passport.use(new LocalStrategy(
         passwordField: 'password'
     },
     function(username, password, done) {
-    
+       
         const uname = username;
         const pwd = password;
-    
-        for(i=0; i<users.length; i++){
-            
+        
+        console.log("asdasd");
+
+        for(i=0; i<users.length(); i++){
             const user = users[i];
-            if(hash === user.password){
-                console.log('LocalStrategy', user);
-                done(null, user);
+            console.log(user);
+            if(uname === user.username){
+                console.log("correct useremail");
+                hasher({password: pwd, salt: user.salt}, 
+                    function (err,pass,salt, hash) {
+                        if(hash === user.password) {
+                            console.log("correct password");
+                            console.log(hash, user.password);
+                            done(null, user);
+                        } else {
+                            done(null, false, { message: 'Incorrect password.'});
+                        }
+                    });
             } else {
-                done(null, false);
+                done(null, false, { message: 'Incorrect email.'});
             }
-        });
-      }
+        }
+        // err 처리
+        done(null, false);
     }
-    done(null, false);
-  }));
+  ));
 
 
 app.get('/', function (req, res) {
 
     if(req.user && req.user.displayName) {
+        console.log('move welcome');
         res.render('welcome.pug', {dname: req.user.displayName});
     } else {
-        res.render('auth.pug');
+        res.render('home.pug');
     }
 });
+
+app.post('/', passport.authenticate('local', {
+    successRedirect: '/welcome',
+    failureRedirect: '/home',
+    failureFlash: false,
+}));
+
+app.get('/welcome', function (req, res) {
+    console.log(req.user, req.user.name);
+    if(req.user && req.user.name) {
+        res.render('welcome.pug', {dname: req.user.name});
+    } else {
+        res.redirect('/');
+    }
+});
+
+app.get('/register', function(req,res) {
+
+    res.render('register.pug');
+})
+
+app.post('/register', function (req, res) {
+
+    console.log(users);
+
+    const uemail = req.body.email;
+    const pwd = req.body.password;
+    const dname = req.body.name;
+    hasher({password: pwd}, function (err, pass, salt, hash) {
+        const user = {
+            useremail: uemail,
+            password: hash,
+            name: dname,
+            salt: salt,
+        }
+        users.push(user);
+        req.login(user, function (err) {
+            req.session.save( function() {
+                res.redirect('/welcome');
+            });
+        });
+    });
+
+    console.log(users);
+});
+
+app.get('/logout', function(req,res) {
+    req.logout();
+    req.session.save(function () {
+        res.redirect('/');
+    });
+});
+
 
 /*
 //passportConfig();
