a2d2f48384928fcabe773ddc19957f43b92df58a
Songhareem <elaha00@naver.com>
2020-01-25 21:17:49 +0900

    basic jwt

--- NodeBird-api/routes/middleware.js
+++ NodeBird-api/routes/middleware.js
@@ -1,6 +1,8 @@
 
 // passport 제공 res 함수 : login logout isAuthenticated
 
+const jwt = require('jsonwebtoken');
+
 exports.isLoggedIn = (req, res, next) => {
     // 로그인 여부 확인
     if(req.isAuthenticated()) {
@@ -15,5 +17,28 @@ exports.isNotLoggedIn = (req, res, next) => {
         next();
     } else {
         res.redirect('/');
+    }
+};
+
+// jwt 검증
+exports.verifyToken = (req, res, next) => {
+
+    try {
+        // jwt.verify(토큰, JWT_SECRET)
+        // 1) 클라에서는 토큰을 authorization HTTP의 헤더에 넣어서 서버로 보냄
+        req.decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+        // 검증 성공시 next, 검증실패시 error
+        return next();
+    } catch (error) {
+        if(error.name === 'TokenExpiredError') {
+            return res.status(419).json({
+                code: 419,
+                message: '토큰이 만료되었습니다' // 유효기간이 만료
+            });
+        }
+        return res.status(401).json({
+            code: 401,
+            message: '유효하지 않은 토큰입니다' //  내가 만든 토큰이 아님
+        })
     }
 };
---
+++ NodeBird-api/routes/v1.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const { verifyToken } = require('./middleware');
+const { Domain, User, Post, Hashtag } = require('../models');
+
+// api 서버의 응답형식은 하나로 통일해주는게 좋음 (json등)
+// 에러 코드를 고유하게 지정해 에러가 뭔지 쉽게 알 수 있게 할 것 
+
+router.post('/token', async(req, res) => {
+    const { clientSecret } = req.body;
+    try {
+        const domain = await Domain.findOne({
+            where: { clientSecret },
+            include : {
+                model: User,
+                attribute: ['nick', 'id'],
+            },
+        });
+        if (!domain) {
+            return res.status(401).json({
+                code: 401,
+                message: '등록되지 않은 도메인입니다. 먼저 도메인을 등록하세요'
+            });
+        }
+    } catch(error) {
+        return res.status(500).json({
+            code: 500,
+            message: '서버 에러',
+        });
+    }
+})
+
+module.exports = router;
